342dd6d8a30fbf784eb7e1ea523d912b54bd6f7c
yyyukina <138771405+yyyukina@users.noreply.github.com>
2023-08-09 14:47:57 +0900

    Math.floorを用いて小数点切り捨て

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,9 +1,15 @@
 window.addEventListener('load', () => {
-    const priceInput = document.getElementById("item-price");
-     priceInput.addEventListener("input", () => {
-       const addTaxDom = document.getElementById("add-tax-price");
-       addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
-       const addPofitDom = document.getElementById("profit");
-       addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
-   })
- });
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+      const addTaxDom = document.getElementById("add-tax-price");
+      const addProfitDom = document.getElementById("profit");
+
+      const inputValue = parseInt(priceInput.value, 10);
+
+      const tax = Math.floor(inputValue * 0.1); 
+      addTaxDom.innerHTML = tax;
+
+      const profit = inputValue - tax;
+      addProfitDom.innerHTML = profit;
+  });
+});
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -100,8 +100,7 @@ RSpec.describe Item, type: :model do
       it 'userが紐づいていなければ登録できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include "User must exist"
-
+        expect(@item.errors.full_messages).to include 'User must exist'
       end
     end
   end
