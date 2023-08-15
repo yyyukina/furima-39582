ae710c178653bc426197f78f9992bb6cfaf09eb4
yyyukina <138771405+yyyukina@users.noreply.github.com>
2023-08-15 21:07:33 +0900

    モデル単体のテストコード

---
+++ spec/factories/order_forms.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :order_form do
+    user_id { Faker::Number.non_zero_digit }
+    item_id { Faker::Number.non_zero_digit }
+    postal_code { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
+    region_id { Faker::Number.between(from: 1, to: 47) }
+    city { Faker::Address.city }
+    address_line { Faker::Address.street_address }
+    building_name { Faker::Address.street_address }
+    phone_number { Faker::Number.decimal_part(digits: 11) }
+    token { Faker::Internet.password(min_length: 20, max_length: 30) }
+  end
+end
