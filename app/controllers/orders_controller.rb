f050d0b68e10b91ae95a78d9e7b9e7df599307e4
yyyukina <138771405+yyyukina@users.noreply.github.com>
2023-08-17 16:25:39 +0900

    画像表示とエラー後の入力修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,6 +15,7 @@ class OrdersController < ApplicationController
            @order_form.save(params,current_user.id)
            redirect_to root_path
         else
+           gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
            render :index, status: :unprocessable_entity
         end
     end
